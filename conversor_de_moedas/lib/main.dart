a0efcc8d6f7169e211e051ba37c0317f9797d3a8
elvesw <elvesw@gmail.com>
2020-04-29 21:57:31 -0300

    app conversor de moedas

---
+++ conversor_de_moedas/lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+const request = "https://api.hgbrasil.com/finance?format=json&key=fa3282ab";
+
+void main() async {
+
+
+  print(await getData());
+  runApp(MaterialApp(
+    home:Home()
+  ));
+}
+
+Future<Map> getData() async{
+  http.Response response =  await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        // ignore: missing_return
+        builder: (context,snapshot){
+          switch(snapshot.connectionState){
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: Text(
+                  "Carregando dados",
+                  style: TextStyle(color: Colors.amber, fontSize: 25.0)
+                ),
+              );
+            default :
+              if(snapshot.hasError){
+                return Center(
+                  child: Text(
+                      "Erro ao carregar dados",
+                      style: TextStyle(color: Colors.amber, fontSize: 25.0)
+                  ),
+                );
+              }else{
+                return Container(
+                  color: Colors.green
+                );
+              }
+          }
+        }
+        )
+    );
+  }
+}
